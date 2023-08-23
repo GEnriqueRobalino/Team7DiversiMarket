986fa8c083bed84648f956d5b074422775610f95
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-08-22 22:47:33 -0500

    Modify

--- 06-Código/03PublicacionesCientificas/src/App.jsx
+++ 06-Código/03PublicacionesCientificas/src/App.jsx
@@ -1,3 +1,5 @@
+//Codificado por: Caisaguano Diana
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PaginaBusqueda from "./paginas/PaginaBusqueda";
